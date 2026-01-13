5692662b30e69aa546f4c43a6a8ff10ee95e2398
Sadulaev <sadulaevismail1999@gmail.com>
2026-01-13 08:10:42 +0300

    updated db init

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,25 +2,21 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User, PaymentSession, BotState } from './entities';
+import configuration from '../config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('database.host'),
-        port: configService.get<number>('database.port'),
-        username: configService.get<string>('database.username'),
-        password: configService.get<string>('database.password'),
-        database: configService.get<string>('database.database'),
-        entities: [User, PaymentSession, BotState],
-        synchronize: configService.get<string>('app.nodeEnv') === 'development',
-        logging: configService.get<string>('app.nodeEnv') === 'development',
-      }),
+   TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: configuration().database.host,
+      port: configuration().database.port,
+      username: configuration().database.username,
+      password: configuration().database.password,
+      database: configuration().database.database,
+      entities: [PaymentSession, User, BotState],
+      synchronize: true,
+      autoLoadEntities: true,
     }),
-    TypeOrmModule.forFeature([User, PaymentSession, BotState]),
   ],
   exports: [TypeOrmModule],
 })
